4d3f966e846d1e2aee16c16dbbc5388bd08c7ebd
leeya018 <leeyahav018@gmail.com>
2024-08-09 10:41:34 +0300

    cahnge the text

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -4,17 +4,20 @@ const MainSection: React.FC = () => {
   return (
     <section
       className="flex flex-col items-center justify-center text-center py-10 bg-background 
-    bg-center border-2 bg-black bg-opacity-40  shadow-lg"
+    bg-center bg-black bg-opacity-40  shadow-lg px-5"
     >
       <h1 className="text-4xl font-bold text-white">הגלריה של שוש</h1>
       <div className="text-2xl font-bold text-white mt-4"> 052-6211512</div>
-      <p className="mt-4 text-2xl text-white max-w-2xl">
-        שמי שוש יהב , אמא ל 3 ילדים מקסימים . מציירת באקרילי על קנבס מאז פרישתי
-        לגמלאות ונהנת מכל רגע , מציירת בעיקר נופים מהטיולים שלי בארץ ובעולם
-        <br /> ברוכים הבאים לגלריה של שוש ,אומנית מוכשרת שמביאה לכם את יופיה של
-        ארץ ישראל באמצעות ציורים מרהיבים באקריליק. כל ציור בגלריה נושא עמו את
-        הרגעים הקסומים והנופים העוצרי נשימה של הארץ – מהגליל הירוק ועד המדבר
-        הזהוב. הכנסו וגלו את האמנות שמעוררת את הלב והנשמה.
+      <p className="mt-4 text-2xl text-white max-w-2xl text-right">
+        שמי שוש , אמא ל 3 ילדים , גרה בלהבים . יצאתי לפרישה מוקדמת לפני ארבע
+        שנים ומאז חלק מעיסוקיי הוא ציור . מציירת באקרילי על קנבס בעיקר נופים
+        מהטיולים שלי מהארץ והעולם ונהנת מכל רגע ,
+      </p>
+      <p className="mt-4 text-2xl text-white max-w-2xl text-right">
+        ברוכים הבאים לגלריה של שוש , אומנית מוכשרת שמביאה לכם את יופיה של ארץ
+        ישראל במקומות אקזוטיים בעולם באמצעות ציורים מרהיבים . כל ציור בגלריה
+        נושא עמו את הרגעים הקסומים והנופים העוצרי נשימה של הארץ – מהגליל הירוק
+        ועד המדבר הזהוב . הכנסו וגלו את האמנות שמעוררת את הלב והנשמה .
       </p>
     </section>
   );
